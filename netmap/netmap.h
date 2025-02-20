573466226667bc0ed6d6e62f2daa5d1b8cad355f
v1rtu3x <marktamas28@gmail.com>
2025-02-20 22:55:55 +0200

    Done some code modifications and implemented basic functionalities

--- netmap/netmap.h
+++ netmap/netmap.h
@@ -1,1 +1,20 @@
 /* netmap.h */
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+
+typedef unsigned char int8;
+typedef unsigned short int16;
+typedef unsigned int int32;
+
+in_addr_t generate(void);
+int main(int, char**);
--- netmap/nm-naive.c
+++ netmap/nm-naive.c
@@ -1,1 +1,51 @@
 /* netmap naive */
+#include "netmap.h"
+
+int16 port;
+in_addr_t current, ending;
+
+in_addr_t generate(){
+
+    if (current >= ending){
+        return (in_addr_t)0;
+    }
+    else
+        current++;
+    return current;
+
+}
+
+//INADDR_NONE
+int main(int argc, char *argv[]){
+    if (argc < 2 ){
+        fprintf(stderr,
+             "Usage: %s <port> [start ip addr] [end ip addr]\n",
+            *argv);
+        return -1;
+    }
+
+    if (argc > 2){
+        current = inet_addr(argv[2]);
+        ending = (argc > 3) ?
+                inet_addr(argv[3]):
+            inet_addr("223.255.254.255");
+        
+        if ((current == INADDR_NONE) 
+        || (ending == INADDR_NONE) 
+        || (ending < current)){
+
+            fprintf(stderr,
+                "Usage: %s <port> [start ip addr] [end ip addr]\n",
+               *argv);
+
+            return -1;
+        }
+        
+    }
+
+    port = (int16)argv[1];
+    printf("Port: %d\nStart: %d\nEnd: %d\n", port, current, ending);
+
+    return 0;
+}
+
